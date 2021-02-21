da8bd523a9d9adb16e3def470726975759de133f
giacomo9999 <gmarchesi888@gmail.com>
2021-02-21 16:18:33 -0500

    Added responsive navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { Link } from "gatsby";
+import oneClubLogo from "../images/logo-small.svg";
+import styles from "./Navbar.module.css";
+console.log("Imported styles: ", styles);
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <nav className={styles.navbar}>
+      <img className={styles.logo} src={oneClubLogo} alt="logo" />
+      <ul className={styles.navlinks}>
+        <li className={styles.navitem}>
+          <Link to="/">AWARDS</Link>
         </li>
-        <li>
-          <Link to="/blog">Blog</Link>
+        <li className={styles.navitem}>
+          <Link to="/blog">PROGRAMS</Link>
         </li>
-        <li>
-          <Link to="/products">Products</Link>
+        <li className={styles.navitem}>
+          <Link to="/products">EVENTS</Link>
+        </li>
+        <li className={styles.navitem}>
+          <Link to="/products">MEMBERSHIPS</Link>
+        </li>
+        <li className={styles.navitem}>
+          <Link to="/products">STORE</Link>
+        </li>
+        <li className={styles.navitem}>
+          <Link to="/products">JOBS</Link>
+        </li>
+        <li className={styles.navitem}>
+          <Link to="/products">ABOUT</Link>
         </li>
       </ul>
     </nav>
@@ -20,3 +36,17 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+{
+  /* <nav class="navbar">
+    <a href="https://freecodecamp.org" class="logo">
+      <img src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg" alt="freeCodeCamp logo">
+    </a>
+    <ul class="nav-links">
+      <li class="nav-item"><a href="#">Curriculum</a></li>
+      <li class="nav-item"><a href="#">Forum</a></li>
+      <li class="nav-item"><a href="#">News</a></li>
+      <li class="nav-item"><a href="#">Sign in</a></li>
+    </ul>
+  </nav> */
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { ExampleButton } from "../components/button";
+import "./index.css";
+import "fontsource-oswald/300.css";
 
 export default () => {
   return (
     <Layout>
       <h1>Hello From Gatsby</h1>
-      <ExampleButton>Click Me!</ExampleButton>
     </Layout>
   );
 };
