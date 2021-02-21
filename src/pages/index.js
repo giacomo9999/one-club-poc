19109072b556bf90615d6f15c16a855a763d0cb6
giacomo9999 <gmarchesi888@gmail.com>
2021-02-21 13:40:26 -0500

    Basic setup complete...getting ready to start.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { ExampleButton } from "../components/button";
 
 export default () => {
   return (
     <Layout>
       <h1>Hello From Gatsby</h1>
+      <ExampleButton>Click Me!</ExampleButton>
     </Layout>
   );
 };
