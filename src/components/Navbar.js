90d9a78094b4d13e38d6a0337ef6a0957137434e
giacomo9999 <gmarchesi888@gmail.com>
2021-02-22 13:55:21 -0500

    Styled navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import oneClubLogo from "../images/logo-small.svg";
-import styles from "./Navbar.module.css";
+import styles from "./Navbar.module.scss";
 console.log("Imported styles: ", styles);
 
 const Navbar = () => {
@@ -9,26 +9,26 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <img className={styles.logo} src={oneClubLogo} alt="logo" />
 
-      <ul className={styles.navlinks}>
-        <li className={styles.navitem}>
+      <ul className={styles.navLinks}>
+        <li className={styles.navItem}>
           <Link to="/">AWARDS</Link>
         </li>
-        <li className={styles.navitem}>
+        <li className={styles.navItem}>
           <Link to="/blog">PROGRAMS</Link>
         </li>
-        <li className={styles.navitem}>
+        <li className={styles.navItem}>
           <Link to="/products">EVENTS</Link>
         </li>
-        <li className={styles.navitem}>
+        <li className={styles.navItem}>
           <Link to="/products">MEMBERSHIPS</Link>
         </li>
-        <li className={styles.navitem}>
+        <li className={styles.navItem}>
           <Link to="/products">STORE</Link>
         </li>
-        <li className={styles.navitem}>
+        <li className={styles.navItem}>
           <Link to="/products">JOBS</Link>
         </li>
-        <li className={styles.navitem}>
+        <li className={styles.navItem}>
           <Link to="/products">ABOUT</Link>
         </li>
       </ul>
@@ -37,17 +37,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-{
-  /* <nav class="navbar">
-    <a href="https://freecodecamp.org" class="logo">
-      <img src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg" alt="freeCodeCamp logo">
-    </a>
-    <ul class="nav-links">
-      <li class="nav-item"><a href="#">Curriculum</a></li>
-      <li class="nav-item"><a href="#">Forum</a></li>
-      <li class="nav-item"><a href="#">News</a></li>
-      <li class="nav-item"><a href="#">Sign in</a></li>
-    </ul>
-  </nav> */
-}
