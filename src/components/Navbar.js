2cd93181d293b3280e198a049d2923cfaf625f12
giacomo9999 <gmarchesi888@gmail.com>
2021-02-22 18:10:28 -0500

    Made navbar dynamic.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,28 @@ import oneClubLogo from "../images/logo-small.svg";
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
+  const linkData = [
+    { name: "AWARDS", linkTo: "/awards" },
+    { name: "PROGRAMS", linkTo: "/programs" },
+    { name: "EVENTS", linkTo: "/events" },
+    { name: "MEMBERSHIPS", linkTo: "/memberships" },
+    { name: "STORE", linkTo: "/store" },
+    { name: "JOBS", linkTo: "/jobs" },
+    { name: "ABOUT", linkTo: "/about" },
+  ];
+
+  const linkArray = linkData.map(entry => (
+    <li key={entry.name}>
+      <Link to={entry.linkTo}>{entry.name}</Link>
+    </li>
+  ));
+  
   return (
     <nav className={styles.navbar}>
       <Link to="/">
         <img className={styles.logo} src={oneClubLogo} alt="logo" />
       </Link>
-
-      <ul>
-        <li>
-          <Link to="/awards">AWARDS</Link>
-        </li>
-        <li>
-          <Link to="/programs">PROGRAMS</Link>
-        </li>
-        <li>
-          <Link to="/events">EVENTS</Link>
-        </li>
-        <li>
-          <Link to="/memberships">MEMBERSHIPS</Link>
-        </li>
-        <li>
-          <Link to="/store">STORE</Link>
-        </li>
-        <li>
-          <Link to="/jobs">JOBS</Link>
-        </li>
-        <li>
-          <Link to="/about">ABOUT</Link>
-        </li>
-      </ul>
+      <ul>{linkArray}</ul>
     </nav>
   );
 };
